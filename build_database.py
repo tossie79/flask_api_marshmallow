cc068482843a637ab6ce414a8b4063f41d6eca53
Tosin Adenaike <tossie79@yahoo.ca>
2025-05-21 12:45:23 +0100

    Initial commit

---
+++ build_database.py
@@ -0,0 +1,55 @@
+# build_database.py
+
+from datetime import datetime
+from config import connex_app, db
+from models import Person, Note
+
+PEOPLE_NOTES = [
+    {
+        "lname": "Fairy",
+        "fname": "Tooth",
+        "notes": [
+            ("I brush my teeth after each meal.", "2022-01-06 17:10:24"),
+            ("The other day a friend said, I have big teeth.", "2022-03-05 22:17:54"),
+            ("Do you pay per gram?", "2022-03-05 22:18:10"),
+        ],
+    },
+    {
+        "lname": "Ruprecht",
+        "fname": "Knecht",
+        "notes": [
+            ("I swear, I'll do better this year.", "2022-01-01 09:15:03"),
+            ("Really! Only good deeds from now on!", "2022-02-06 13:09:21"),
+        ],
+    },
+    {
+        "lname": "Bunny",
+        "fname": "Easter",
+        "notes": [
+            ("Please keep the current inflation rate in mind!", "2022-01-07 22:47:54"),
+            ("No need to hide the eggs this time.", "2022-04-06 13:03:17"),
+        ],
+    },
+]
+
+if __name__ == "__main__":
+    print("Starting build_database.py...")
+
+    with connex_app.app.app_context():
+        print("Dropping and recreating all tables...")
+        db.drop_all()
+        db.create_all()
+        print("Tables created successfully.")
+
+        for data in PEOPLE_NOTES:
+            new_person = Person(lname=data["lname"], fname=data["fname"])
+            for content, timestamp in data.get("notes", []):
+                new_person.notes.append(
+                    Note(
+                        content=content,
+                        timestamp=datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"),
+                    )
+                )
+            db.session.add(new_person)
+        db.session.commit()
+        print("Database seeded successfully.")
